30c54921669118965df50701ad1c25aa1b3962e4
杨光 <yangguang112@crland.com.cn>
2022-02-24 09:42:14 +0800

    kotlin配置

--- moduleaa/src/main/java/com/crlandpm/moduleaa/ModuleaActivity.kt
+++ moduleaa/src/main/java/com/crlandpm/moduleaa/ModuleaActivity.kt
@@ -2,12 +2,13 @@ package com.crlandpm.moduleaa
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.alibaba.android.arouter.facade.annotation.Route
 
 /**
  * Author: Danny Yang
  * Date: 2022/02/22/2:38 下午
  */
-
+@Route(path = "/componenta/moduleaActivity")
 class ModuleaActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
