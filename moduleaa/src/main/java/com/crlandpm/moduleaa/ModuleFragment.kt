8903e7078e86db118eae76cf841359ca2bf54a16
杨光 <yangguang112@crland.com.cn>
2022-03-02 20:55:32 +0800

    添加Fragment

--- moduleaa/src/main/java/com/crlandpm/moduleaa/ModuleFragment.kt
+++ moduleaa/src/main/java/com/crlandpm/moduleaa/ModuleFragment.kt
@@ -5,12 +5,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.alibaba.android.arouter.facade.annotation.Route
+import com.crlandpm.module_export.ARouterTable
 
 /**
  * Author: Danny Yang
  * Date: 2022/02/22/2:46 下午
  */
 
+@Route(path = ARouterTable.PAth_FRAGMENT_A)
 class ModuleFragment : Fragment() {
 
     override fun onCreateView(
